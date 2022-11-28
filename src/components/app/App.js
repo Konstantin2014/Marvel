fbe100ca2d0e75ac5f92ab6b6e119f1b1355197d
Konstantin2014 <87135556+Konstantin2014@users.noreply.github.com>
2022-11-28 13:35:44 +0200

    start

---
+++ src/components/app/App.js
@@ -0,0 +1,24 @@
+import AppHeader from "../appHeader/AppHeader";
+import CharList from "../chatList/CharList";
+import CharInfo from "../charInfo/CharInfo";
+import RandomChar from "../randomChar/RandomChar";
+
+import decoration from "../../resources/img/vision.png";
+
+function App() {
+  return (
+    <div className="app">
+      <AppHeader />
+      <main>
+        <RandomChar />
+        <div className="char__content">
+          <CharList />
+          <CharInfo />
+        </div>
+        <img className="bg-decoration" src={decoration} alt="vision" />
+      </main>
+    </div>
+  );
+}
+
+export default App;
