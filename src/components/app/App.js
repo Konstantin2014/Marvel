7cc4a3fcf5fbe0fe1e0ef4fd31cb8af6a0540be0
Konstantin2014 <87135556+Konstantin2014@users.noreply.github.com>
2022-11-28 16:58:47 +0200

    state in rendomChar

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -2,14 +2,8 @@ import AppHeader from "../appHeader/AppHeader";
 import CharList from "../chatList/CharList";
 import CharInfo from "../charInfo/CharInfo";
 import RandomChar from "../randomChar/RandomChar";
-import MarvelService from "../../services/MarvelService";
 
 import decoration from "../../resources/img/vision.png";
-
-// const marvelService = new MarvelService();
-// marvelService
-//   .getAllCharacters()
-//   .then((res) => res.data.results.forEach((item) => console.log(item.name)));
 
 function App() {
   return (
